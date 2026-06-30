fb7be00ac49b95f989ee5ac4b97cf0c76687766a
anagha-06 <anagha.anurags@gmail.com>
2026-06-30 22:10:29 +0530

    Update retrieve.py

--- rag_pipeline/retrieve.py
+++ rag_pipeline/retrieve.py
@@ -1,15 +1,13 @@
 import chromadb
 from google import genai
-
-import os
 from dotenv import load_dotenv
-from google import genai
+import os
 
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
+client_gemini = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
 client_chroma = chromadb.PersistentClient(path="chroma_db")
 
@@ -17,11 +15,8 @@ collection = client_chroma.get_collection(
     name="college_documents"
 )
 
-while True:
-    question = input("\nAsk a question (type 'exit' to quit): ")
 
-    if question.lower() == "exit":
-        break
+def generate_answer(question):
 
     query_embedding = client_gemini.models.embed_content(
         model="gemini-embedding-001",
@@ -29,41 +24,48 @@ while True:
     )
 
     results = collection.query(
-        query_embeddings=[
-            query_embedding.embeddings[0].values
-        ],
+        query_embeddings=[query_embedding.embeddings[0].values],
         n_results=3
     )
-    
+
+    if not results["documents"] or not results["documents"][0]:
+        return {
+            "answer": "Information not found in the provided documents.",
+            "sources": []
+        }
+
     documents = results["documents"][0]
     metadata = results["metadatas"][0]
-    
+
     context = ""
+
     for i, doc in enumerate(documents):
         context += f"Context {i+1}:\n{doc}\n\n"
 
     prompt = f"""
-    You are a college information assistant.
+You are a college information assistant.
 
-    Answer ONLY using the provided context.
-    
-    you can also answer basic questions which are part of the conversation like hi, hello, how are you and so on.
+Answer ONLY using the provided context.
 
-    If the answer is not available in the context, say:
-    "Information not found in the provided documents."
+If the answer is not available, reply:
+Information not found in the provided documents.
 
-    Context:
-     {context}
+Context:
+{context}
 
-    Question:
-     {question}
-    """
+Question:
+{question}
+"""
 
     response = client_gemini.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt
     )
 
-    print("\nAnswer:")
-    print(response.text)
+    sources = list(set(meta["source"] for meta in metadata))
+
+    return {
+        "answer": response.text,
+        "sources": sources
+    }
     
